350dda68f078b5d773d712c25f8504716d658fe9
marco sameh <marcosameh678@gmail.com>
2022-02-07 21:16:27 +0200

    BERO

--- App.UI/Pages/admin/Index.cshtml.cs
+++ App.UI/Pages/admin/Index.cshtml.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace App.UI.Pages.Admin
 {
     [Authorize(Roles = "Admin")]
     public class IndexModel : PageModel
     {
+
         public void OnGet()
         {
+           
         }
     }
 }
