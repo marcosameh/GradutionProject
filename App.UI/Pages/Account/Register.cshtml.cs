3835aec9bb769db634edd60bc1ce9a34ac81de5d
marco sameh <marcosameh678@gmail.com>
2022-02-08 15:46:09 +0200

    Solve register problem

--- App.UI/Pages/Account/Register.cshtml.cs
+++ App.UI/Pages/Account/Register.cshtml.cs
@@ -98,31 +98,15 @@ namespace App.UI.Areas.Identity.Pages.Account
                 var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, Photo = photoname };
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                user.EmailConfirmed = true;
                 await _userManager.AddToRoleAsync(user, "Admin");
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("/Account/RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
+                      
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return RedirectToPage("Login");
-                    }
+                    
 
                 }
                 foreach (var error in result.Errors)
