3a22a9d5d79e2b1f8b7c4caae9482e526b5cb0a1
marco sameh <marcosameh678@gmail.com>
2022-02-25 21:53:48 +0200

    Datatable

---
+++ App.UI/Controllers/AuthorController.cs
@@ -0,0 +1,59 @@
+﻿using App.Librarian.Managers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+namespace App.UI.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+
+        private readonly AuthorManager authorManager;
+
+        public AuthorController(AuthorManager authorManager)
+        {
+
+            this.authorManager = authorManager;
+        }
+        [HttpPost]
+        
+        public IActionResult GetAuthors()
+        {
+            try
+            {
+                var draw = Request.Form["draw"].FirstOrDefault();
+                var start = Request.Form["start"].FirstOrDefault();
+                var length = Request.Form["length"].FirstOrDefault();
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+                var customerData = authorManager.GetAuthors().AsQueryable();
+                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+                {
+                    customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection);
+                }
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    customerData = customerData.Where(m => m.Name.Contains(searchValue)
+                                                || m.Bio.Contains(searchValue));
+                }
+                recordsTotal = customerData.Count();
+                var data = customerData.Skip(skip).Take(pageSize).ToList();
+                var jsonData = new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+                return Ok(jsonData);
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
