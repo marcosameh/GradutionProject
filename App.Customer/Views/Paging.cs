1186b2be2078ff6f77e21c0d581b98ec04713d8a
peterezzat <55260283+peterezz@users.noreply.github.com>
2022-03-15 23:44:35 +0200

    updates

---
+++ App.Customer/Views/Paging.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Customer.Views
+{
+    public class Paging
+    {
+        public int ItemNum{ get; set; }
+
+     
+     
+        public int ItemPerPage { get; }
+
+        public Paging( int ItemPerPage)
+        {
+           
+            this.ItemPerPage = ItemPerPage;
+        }
+        public int GetNumOfPages()
+        {
+            if(ItemNum % ItemPerPage==0)
+            {
+                return (ItemNum / ItemPerPage);
+            }
+            return (ItemNum / ItemPerPage) + 1;
+        }
+        public int SkipNumBooks(int CurrentPage)
+        {
+            return (CurrentPage - 1) * ItemPerPage;
+        }
+    }
+}
