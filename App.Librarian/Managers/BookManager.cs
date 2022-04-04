7bc6c9b3ad92acb37adfb7bffe6a1742783b4a90
peterezzat <55260283+peterezz@users.noreply.github.com>
2022-04-04 20:14:06 +0200

    fix some links

--- App.Librarian/Managers/BookManager.cs
+++ App.Librarian/Managers/BookManager.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace App.Librarian.Managers
 {
-    public class 
+    public class BookManager
 
     {
         private readonly IMapper mapper;
@@ -80,12 +80,12 @@ namespace App.Librarian.Managers
             var Book = BookRepo.GetMany(book => book.Offer != null && book.IsActive == true, book => book.Author).Take(take).ToList();
             return mapper.Map<List<BookVM>>(Book);
         }
-        //public List<BookVM> GetMostSellingBook()
-        //{
+        public List<BookVM> GetMostSellingBook()
+        {
 
-        //    var Book = BookRepo.GetAll().OrderByDescending(book=>book.NumSells).Take(take).ToList();
-        //    return mapper.Map<List<BookVM>>(Book);
-        //}
+            var Book = BookRepo.GetAll().OrderByDescending(book => book.NumSells).Take(take).ToList();
+            return mapper.Map<List<BookVM>>(Book);
+        }
         public List<BookVM> GetNewArrivalls()
         {
             var Book = BookRepo.GetAll().OrderByDescending(book=>book.Id).ToList();
