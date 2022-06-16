4a4c98e36604432e50b6c19b88d5fcd5b84967af
Marco Sameh <marcosameh678@gmail.com>
2022-06-16 16:10:49 +0200

    Admin

---
+++ App.UI/Pages/Bookstores/Iist.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.UI.Pages.Bookstores
+{
+    public class IistModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ App.UI/Pages/Bookstores/delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.UI.Pages.Bookstores
+{
+    public class deleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
